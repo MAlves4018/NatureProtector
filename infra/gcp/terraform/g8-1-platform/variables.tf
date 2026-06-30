9011e8b0cfc71e50d91306e11240e0819d65b702
Miguel ALves <migkxl@gmail.com>
2026-06-30 04:35:45 +0100

    Grant deployer access to all Cloud Deploy source buckets (#41)

--- infra/gcp/terraform/g8-1-platform/variables.tf
+++ infra/gcp/terraform/g8-1-platform/variables.tf
@@ -71,7 +71,27 @@ variable "cloud_deploy_source_bucket_name" {
 
   validation {
     condition     = var.cloud_deploy_source_bucket_name == "d09bb0b9ead342f0a6b38ecd9db4c11a_clouddeploy"
-    error_message = "Unexpected Cloud Deploy source bucket."
+    error_message = "Unexpected Cloud Deploy API source bucket."
+  }
+}
+
+variable "cloud_deploy_frontend_source_bucket_name" {
+  type    = string
+  default = "0055c8c327b743efbfa1809f2a4363ef_clouddeploy"
+
+  validation {
+    condition     = var.cloud_deploy_frontend_source_bucket_name == "0055c8c327b743efbfa1809f2a4363ef_clouddeploy"
+    error_message = "Unexpected Cloud Deploy frontend source bucket."
+  }
+}
+
+variable "cloud_deploy_prevention_source_bucket_name" {
+  type    = string
+  default = "c31effabdcbc4c0895cf09390ae59db0_clouddeploy"
+
+  validation {
+    condition     = var.cloud_deploy_prevention_source_bucket_name == "c31effabdcbc4c0895cf09390ae59db0_clouddeploy"
+    error_message = "Unexpected Cloud Deploy prevention source bucket."
   }
 }
 
